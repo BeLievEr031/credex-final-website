c14a95ba309bf63cd5f43fcd47cec101b649af31
BeLievEr031 <sandyrajak031@gmail.com>
2026-05-22 07:00:03 +0530

    add new files

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -75,23 +75,24 @@ function Blog() {
                         </div>
                     </div>
 
-                    <div className="hidden md:block py-12 md:py-3 bg-[#1212120A] md:px-4 rounded-xl space-y-2 cursor-pointer" onClick={() => navigate(`${"how-to-build-voice-ai-without-burning-through-your-budget-500k-in-deepgram-credits-at-50-off"}`)}>
+                    <div className="hidden md:block py-12 md:py-3 bg-[#1212120A] md:px-4 rounded-xl space-y-2 cursor-pointer" onClick={() => navigate(`${"how-to-build-ai-powered-products-without-burning-developer-budget"}`)}>
                         <p className="text-2xl font-semibold font-pp-mori-semibold w-[75%]">
-                            How to Build Voice AI Without Burning Through Your Budget: $500K in Deepgram Credits at 50% Off
+                            How to Build AI-Powered Products Without Burning Developer Budget
                         </p>
                         <div className="text-sm text-[#19363FB2]">
-                            <p>Credex - 03rd April, 2026</p>
+                            <p>Credex - 22nd April, 2026</p>
                         </div>
                         {/* <p className="text-[#19363FB2] text-[20px]">Create superior code, compose emails, boost any kind of work within a collaborative team.</p> */}
 
 
                         <div className="w-full md:w-[520px] h-[230px] md:h-[281px] bg-[#1C1C1C] rounded-xl mt-6 overflow-hidden">
-                            <img src="https://miro.medium.com/v2/resize:fit:788/1*I3RDF2JS_Tg0LObY45vgPw.png" alt="" className="w-full h-full object-cover object-top" />
+                            <img src="https://miro.medium.com/v2/resize:fit:788/1*ZrAsb8CoeQAyTVkYueMmyg.png" alt="" className="w-full h-full object-cover object-top" />
                         </div>
                     </div>
                 </div>
 
                 <div className="grid md:grid-cols-3 3xl:grid-cols-4 pt-8 md:pt-24 gap-x-5 gap-y-5">
+                    <BlogCard11 />
                     <BlogCard10 />
                     <BlogCard9 />
                     <BlogCard8 />
@@ -308,6 +309,31 @@ const BlogCard10 = () => {
 
         <div className="flex items-center gap-2 text-sm text-[#19363FB2] pt-2">
             <p>Credex • 03rd April, 2026</p>
+        </div>
+    </div>
+}
+const BlogCard11 = () => {
+    const navigate = useNavigate()
+    return <div
+        className="cursor-pointer w-full max-w-[378px]"
+        onClick={() =>
+            navigate("how-to-build-ai-powered-products-without-burning-developer-budget")
+        }
+    >
+        <div className="bg-[#121212] w-full h-[200px] sm:h-[220px] md:h-[230px] rounded-[20px] overflow-hidden">
+            <img
+                src="https://miro.medium.com/v2/resize:fit:788/1*ZrAsb8CoeQAyTVkYueMmyg.png"
+                alt=""
+                className="w-full h-full object-cover object-top"
+            />
+        </div>
+
+        <p className="text-lg sm:text-xl md:text-2xl font-semibold pt-4 w-full">
+            How to Build AI-Powered Products Without Burning Developer Budget
+        </p>
+
+        <div className="flex items-center gap-2 text-sm text-[#19363FB2] pt-2">
+            <p>Credex • 22nd April, 2026</p>
         </div>
     </div>
 }
